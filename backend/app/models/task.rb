c8ed34e55e5118cb6a436fd106ece01123b9f000
ekity1002 <dreamguidance1@gmail.com>
2024-02-24 21:58:25 +0900

    add tasj status

--- backend/app/models/task.rb
+++ backend/app/models/task.rb
@@ -4,5 +4,6 @@ class Task < ApplicationRecord
   belongs_to :project, optional: true
   has_many :task_tags
   has_many :tags, through: :task_tags
+  enum status: { incomplete: false, complete: true }
 end
 
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_24_071846) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_24_122931) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_24_071846) do
     t.bigint "project_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "status", default: false
     t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
